1a118455b815573f4c208d7791cd534f78ab8313
coolasfk <fk.przybyla@gmail.com>
2023-07-05 17:42:22 +0200

    edit-your-profile

--- components/EditYourProfile.js
+++ components/EditYourProfile.js
@@ -27,5 +27,77 @@ const Design = StyleSheet.create({
     color: Color.fontBodyColor,
   },
 });
+const design = StyleSheet.create({
+  inputContainer: {
+    flexDirection: "row",
+    // justifyItems: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    // borderWidth: 1,
+    margin: 10,
+    backgroundColor: Color.color3,
+
+    borderRadius: 45,
+    padding: 15,
+    maxWidth: 500,
+  },
+  textInput: {
+    color: Color.color2,
+    textAlign: "center",
+    fontFamily: "Quicksand_500Medium",
+    fontSize: 18,
+    // fontSize: 22,
+    maxWidth: 300,
+    backgroundColor: Color.color3,
+
+    marginRight: 10,
+
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  button: {
+    marginLeft: 100,
+  },
+  smallText: {
+    color: Color.color2,
+    textAlign: "center",
+    fontFamily: "Quicksand_500Medium",
+    fontSize: 18,
+    maxWidth: 300,
+  },
+  centered: {
+    overflow: "visible",
+    marginTop: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "top",
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#C7D8C5",
+    paddingTop: 50,
+  },
+  inner: {
+    padding: 24,
+    flex: 1,
+    justifyContent: "space-around",
+  },
+
+  textInput: {
+    height: 40,
+    borderColor: "#000000",
+    borderBottomWidth: 1,
+    marginBottom: 36,
+  },
+  btnContainer: {
+    backgroundColor: "white",
+    marginTop: 12,
+  },
+});
 
 export default EditYourProfile;
